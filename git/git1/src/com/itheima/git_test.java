1197bec31111bcf72d06417cb0d2b6616ecd506b
hancongcong <695476486@qq.com>
2019-06-16 20:18:31 +0800

    新增输出

---
+++ git/git1/src/com/itheima/git_test.java
@@ -0,0 +1,12 @@
+package com.itheima;
+
+public class git_test {
+    public static void main(String[] args) {
+        System.out.println("sssssssssssssssss");
+        System.out.println("sssssssssssssssss");
+        System.out.println("sssssssssssssssss");
+        System.out.println("sssssssssssssssss");
+        System.out.println("sssssssssssssssss");
+        System.out.println("sssssssssssssssss");
+    }
+}
