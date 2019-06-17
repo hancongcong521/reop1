c7d80b2f094e1f01de3459d1f59b2e3894455cdc
hancongcong <695476486@qq.com>
2019-06-17 08:47:16 +0800

    定义了一个i

--- git/git1/src/com/itheima/git_test.java
+++ git/git1/src/com/itheima/git_test.java
@@ -2,9 +2,9 @@ package com.itheima;
 
 public class git_test {
     public static void main(String[] args) {
-        System.out.println("1111111111");
-
-        System.out.println("aaaaaaaaaaa");
-
+        int i = 1;
+        i += 10;
+        System.out.println(i);
     }
+
 }
