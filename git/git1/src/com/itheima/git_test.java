1f1632fa7f975e16b0f338a7768bfa03bbbe533f
hancongcong <695476486@qq.com>
2019-06-17 09:25:56 +0800

    定义了888888

--- git/git1/src/com/itheima/git_test.java
+++ git/git1/src/com/itheima/git_test.java
@@ -2,6 +2,9 @@ package com.itheima;
 
 public class git_test {
     public static void main(String[] args) {
-        System.out.println("888888888888");
+        System.out.println("1111111111");
+
+        System.out.println("aaaaaaaaaaa");
+
     }
 }
